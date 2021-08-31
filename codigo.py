38b83a26ad977d03cf3cfde5f1adff2e8916739a
Alejandro Gómez <sagomeza@correo.udistrital.edu.co>
2021-08-31 07:43:05 -0500

    Toca arreglar un problema porque se estaba llamando una función donde no existia esta, pendiente de revisar

--- codigo.py
+++ codigo.py
@@ -34,12 +34,14 @@ KEYS2 = {'a': '00', 'b': '01', 'c': '02', 'd': '03', 'e': '04', 'f': '05',
          'W': '48', 'X': '49', 'Y': '50', 'Z': '51', '0': '52', '1': '53',
          '2': '54', '3': '55', '4': '56', '5': '57', '6': '58', '7': '59',
          '8': '60', '9': '61', '.': '62', ',': '63', '?': '64', '!': '65',
-         ' ': '66', '\n': '67' }
+         ' ': '66', '\n': '67'}
 
 '''
-Clase encargada de generar una clave de acuerdo a los parametros 
+Clase encargada de generar una clave de acuerdo a los parametros
 iniciales, los cuales le daran la complejidad a la clave
 '''
+
+
 class Clave:
     def __init__(self):
         #Valores iniciales que determinan la complejidad de la clave
@@ -51,7 +53,7 @@ class Clave:
         #input: a=Número entero , b=Número entero
         #output: mcd entre a y b  , 1 = a*u0 + b*v0
         if b == 0:
-            return 0,1,0
+            return 0, 1, 0
         u0 = 1
         u1 = 0
         v0 = 0
@@ -69,12 +71,12 @@ class Clave:
             u1 = u
             v0 = v1
             v1 = v
-        return  a, u0, v0
+        return a, u0, v0
 
     def _modulo_inverso(self, num):
         #Hallamos el inverso mod(self.p-1) del parametro
         mod = self.p - 1
-        mcd,u,v = self._algoritmo_de_Euclides(mod,num)
+        mcd, u, v = self._algoritmo_de_Euclides(mod, num)
         if mcd != 1:
             print('No existe inverso')
             return 0
@@ -91,15 +93,15 @@ class Clave:
                 busqueda = False
                 return e
 
-    def _primo_long_impar(self,bits):
-        #Generamos un número primo de acuerdo al parametro, 
+    def _primo_long_impar(self, bits):
+        #Generamos un número primo de acuerdo al parametro,
         #con una cantidad impar de digitos
         p = 'UD'
         while len(str(p)) % 2 == 0:
             p = self._generar_primo(bits)
         return p
 
-    def _generar_primo(self,bits):
+    def _generar_primo(self, bits):
         #Generamos un número primo de acuerdo al parametro
         busqueda = True
         while busqueda:
@@ -112,22 +114,26 @@ class Clave:
             if es_primo:
                 return p
 
+
 '''
-Esta clase e encarga de almacenar el texto y las funciones necesarias 
+Esta clase e encarga de almacenar el texto y las funciones necesarias
 para cifrar o decifrar el texto almacenado, en base a KEYS2
-'''     
+'''
+
+
 class Mensaje:
     #Valores iniciales, los cuales constan de un texto y de un entero p
-    def __init__(self,texto,p):
+    def __init__(self, texto, p):
         self.texto = texto
         self.p = p
 
-    def cifrar_mensaje(self,e):
+    def cifrar_mensaje(self, e):
         #Recibe el parametro e, con esto se prodece a pasar la cadena de caracteres a una cadena de enteros
         #y son codificados por el parametro e
         lista_enteros = self._str_a_int()
         enteros = [int(num) for num in lista_enteros if True]
-        enteros_cifrados = [self._calcular_Pe_mod_p(num, e, self.p) for num in enteros if True]
+        enteros_cifrados = [self._calcular_Pe_mod_p(
+            num, e, self.p) for num in enteros if True]
         lista_enteros_cifrados = [str(num) for num in enteros_cifrados if True]
         texto_cifrado = ''
         for idx in range(len(lista_enteros_cifrados)):
@@ -145,10 +151,10 @@ class Mensaje:
         for n in range(0, len(bin_e)):
             if n == 0:
                 C = P % p
-                potencias_P.insert(0,C)
+                potencias_P.insert(0, C)
             else:
                 C = potencias_P[0]**2 % p
-                potencias_P.insert(0,C)
+                potencias_P.insert(0, C)
         resultado = 1
         for i in range(len(bin_e)):
             if bin_e[i] == 1:
@@ -161,8 +167,8 @@ class Mensaje:
         longitud = len(str(self.p))
         lista_enteros_cifrados = []
         grupo = ''
-        for id in range(1,len(self.texto)):
-            if len(grupo) < longitud :
+        for id in range(1, len(self.texto)):
+            if len(grupo) < longitud:
                 grupo += self.texto[id - 1]
             else:
                 lista_enteros_cifrados.append(grupo)
@@ -170,11 +176,12 @@ class Mensaje:
         grupo += self.texto[-1]
         lista_enteros_cifrados.append(grupo)
         try:
-            enteros_cifrados = [int(num) for num in lista_enteros_cifrados if True]
+            enteros_cifrados = [int(num) for num in lista_enteros_cifrados]
         except ValueError:
             print('Valores ingresados no son correctos!')
-            run()
-        enteros = [self._calcular_Pe_mod_p(num, d, self.p) for num in enteros_cifrados if True]
+
+        enteros = [self._calcular_Pe_mod_p(
+            num, d, self.p) for num in enteros_cifrados if True]
         lista_enteros = [str(num) for num in enteros if True]
         texto_decifrado = ''
         for idx in range(len(lista_enteros)):
@@ -208,9 +215,10 @@ class Mensaje:
         #Apartir de una cadena de enteros, retorna una cadena de caracteres
         longitud = len(texto_decifrado)//2
         texto_final = ''
-        letras_num = [texto_decifrado[i*2 : (i + 1)*2] for i in range(longitud) if True]
+        letras_num = [texto_decifrado[i*2: (i + 1)*2]
+                      for i in range(longitud) if True]
         for num in letras_num:
-            for k , v in KEYS.items():
+            for k, v in KEYS.items():
                 if v == num:
                     texto_final += k
         return texto_final
