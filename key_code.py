28903708a4120adf5d291116dfe830321085147e
Alejandro Gómez <sagomeza@correo.udistrital.edu.co>
2022-01-28 10:51:12 -0500

    Agregada el algoritmo de euclides al módulo key_code

--- key_code.py
+++ key_code.py
@@ -1,11 +1,17 @@
 import math
-from random import random
+import random
 
 def odd_length(func):
-    pass
+    def wrapper(*args, **kwargs):
+        p = func(*args, **kwargs)
+        while len(str(p)) % 2 == 0:
+            p = func(*args, **kwargs)
+        return p
+    return wrapper
 
-def generate_prime(bits):
-
+@odd_length
+def generate_prime(bits: int) -> int: 
+    # This function generates a prime number
     search = True
     while search:
         is_prime = True
@@ -16,6 +22,29 @@ def generate_prime(bits):
                 break
         if is_prime:
             return p
+
+def euclidean_algorithm(a: int, b: int) -> tuple:
+    # output: gcd , n , m where gcd = a*n + b*m
+    if b == 0:
+        return 0,1,0
+    u0 , u1 = 1 , 0
+    v0 , v1 = 0 , 1
+    while b != 0:
+        q = a//b
+        r = a - b*q
+        u = u0 - q*u1
+        v = v0 - q*v1
+        # Update a,b
+        a , b = b , r
+        # Update for next iteration
+        u0 , u1 = u1 , u
+        v0 , v1 = v1 , v
+    return a , u0 , v0
+
+
+
+
+
 
 
 
