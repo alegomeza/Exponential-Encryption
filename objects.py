5867bf80088decbb27e503ead268b75aad5785d0
alegomeza <a.gomeza@hotmail.com>
2024-11-13 22:46:13 -0500

    ya esta implementado el código para cifrar y decifrar, sin embargo toca ajustar detalles, se adjunta una particularidad para estudiar el final del código comentando

--- objects.py
+++ objects.py
@@ -281,7 +281,6 @@ class Encryp:
             print(f"{cypher_line=}")
             cypher_message.append(cypher_line)
             print(f"{cypher_message=}")
-        print("#"*30)
         return Message(cypher_message)
     
     def decryp_message(self, cypher_message: Message) -> Message:
@@ -300,9 +299,18 @@ class Encryp:
             print(f"{cypher_number_line_split=}")
             number_line_split = self._to_decrypt(cypher_number_line_split)
             print(f"{number_line_split=}")
-            input("#"*20)
-            ...
-        ...
+            number_str_line_split = self._add_zeros(number_line_split)
+            print(f"{number_str_line_split=}")
+            # number_str_line_split = self._list_int_to_str(number_line_split)
+            number_str_line = split_join.join(number_str_line_split)
+            print(f"{number_str_line=}")
+            number_line = int(number_str_line)
+            print(f"{number_line=}")
+            line = self._to_str(number_line)
+            print(f"{line=}")
+            message.append(line)
+            print(f"{message=}")
+        return Message(message)
 
 
     def _to_int(self, line: str) -> int:
@@ -315,12 +323,12 @@ class Encryp:
         return [self.key.convert(num=number, mode="encrypt")
                 for number in number_line_split]
 
-    def _to_decrypt(self, cypher_number_line_split: List[int]) -> List[int]:
+    def _to_decrypt(self, number_line_split: List[int]) -> List[int]:
         return [self.key.convert(num=number, mode="decrypt")
-                for number in cypher_number_line_split]
+                for number in number_line_split]
         
-    def _add_zeros(self, cypher_number_line_split: List[int]) -> List[str]:
-        return [self._zeros(num) for num in cypher_number_line_split]
+    def _add_zeros(self, number_line_split: List[int]) -> List[str]:
+        return [self._zeros(num) for num in number_line_split]
         
     def _zeros(self, num: int) -> str:
         num_str = str(num)
@@ -328,9 +336,11 @@ class Encryp:
             num_str = "0" + num_str
         return num_str
     
-    def _list_str_to_int(self, cypher_number_str_line_split: List[str]) -> List[int]:
-        return [int(number) for number in cypher_number_str_line_split]
-        
+    def _list_str_to_int(self, number_str_line_split: List[str]) -> List[int]:
+        return [int(number) for number in number_str_line_split]
+    
+    def _list_int_to_str(self, number_line_split: List[str]) -> List[str]:
+        return [str(number) for number in number_line_split]        
         
 
 
@@ -348,3 +358,41 @@ if __name__ == "__main__":
         ...
     except:
         input("ERROR")
+        
+# Sebastián Alejandro Gómez Ardila
+
+#  #################### 
+
+# Key(k1=1409, k2=769, k3=769)
+
+#  ####################
+
+# line='Sebastián Alejandro Gómez Ardila'
+# number_line=76747180370329892177287290112450270374829442316474700438594099
+# number_str_line='76747180370329892177287290112450270374829442316474700438594099'
+# number_str_line_split=['767', '471', '803', '703', '298', '921', '772', '872', '901', '124', '502', '703', '748', '294', '423', '164', '747', '004', '385', '940', '99']
+# number_line_split=[767, 471, 803, 703, 298, 921, 772, 872, 901, 124, 502, 703, 748, 294, 423, 164, 747, 4, 385, 940, 99]
+# cypher_number_line_split=[847, 19, 760, 593, 1027, 932, 27, 67, 939, 1233, 1151, 593, 592, 1394, 408, 418, 1134, 25, 154, 1131, 952]
+# cypher_number_str_line_split=['0847', '0019', '0760', '0593', '1027', '0932', '0027', '0067', '0939', '1233', '1151', '0593', '0592', '1394', '0408', '0418', '1134', '0025', '0154', '1131', '0952']
+# cypher_number_str_line='084700190760059310270932002700670939123311510593059213940408041811340025015411310952'
+# cypher_number_line=84700190760059310270932002700670939123311510593059213940408041811340025015411310952
+# cypher_line='%Ó.JZwG)"{rnc(*E;}¿¿tk@qlÓ#UTRWLÍsÍj{DiGÉ L'
+# cypher_message=['%Ó.JZwG)"{rnc(*E;}¿¿tk@qlÓ#UTRWLÍsÍj{DiGÉ L']
+# %Ó.JZwG)"{rnc(*E;}¿¿tk@qlÓ#UTRWLÍsÍj{DiGÉ L
+
+#  ####################
+
+# cypher_line='%Ó.JZwG)"{rnc(*E;}¿¿tk@qlÓ#UTRWLÍsÍj{DiGÉ L'
+# cypher_number_line=84700190760059310270932002700670939123311510593059213940408041811340025015411310952
+# cypher_number_str_line='84700190760059310270932002700670939123311510593059213940408041811340025015411310952'
+# cypher_number_str_line_split=['8470', '0190', '7600', '5931', '0270', '9320', '0270', '0670', '9391', '2331', '1510', '5930', '5921', '3940', '4080', '4181', '1340', '0250', '1541', '1310', '952']
+# cypher_number_line_split=[8470, 190, 7600, 5931, 270, 9320, 270, 670, 9391, 2331, 1510, 5930, 5921, 3940, 4080, 4181, 1340, 250, 1541, 1310, 952]
+# number_line_split=[625, 483, 985, 118, 675, 756, 675, 266, 1079, 1184, 279, 1394, 1017, 1194, 3, 827, 1330, 40, 1180, 457, 99]
+# number_str_line_split=['0625', '0483', '0985', '0118', '0675', '0756', '0675', '0266', '1079', '1184', '0279', '1394', '1017', '1194', '0003', '0827', '1330', '0040', '1180', '0457', '0099']
+# number_str_line='062504830985011806750756067502661079118402791394101711940003082713300040118004570099'
+# number_line=62504830985011806750756067502661079118402791394101711940003082713300040118004570099
+# line="AnfVIVññ?mVsé'EH¿Z¿WD%íqHcqS]ÉLT&fáYÚ;kC{cI"
+# message=["AnfVIVññ?mVsé'EH¿Z¿WD%íqHcqS]ÉLT&fáYÚ;kC{cI"]
+# AnfVIVññ?mVsé'EH¿Z¿WD%íqHcqS]ÉLT&fáYÚ;kC{cI
+
+#  ####################
